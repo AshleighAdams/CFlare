cc479fadf27b731f4c504cbe6cb29bfaf13531a3
Kate Adams <self@kateadams.eu>
2015-02-08 17:59:37 +0000

    indents.

--- src/cflare/hash.c
+++ src/cflare/hash.c
@@ -7,19 +7,19 @@ cflare_hash cflare_hash_compute(const void* data, size_t len)
 	ret.pointer = data;
 	ret.pointer_size = len;
 	ret.hash = 0;
-	
+
 	const char* ptr = (const char*)data;
-    uint32_t computed, i;
-    for(computed = i = 0; i < len; i++)
-    {
-        computed += ptr[i];
-        computed += (computed << 10);
-        computed ^= (computed >> 6);
-    }
-    computed += (computed << 3);
-    computed ^= (computed >> 11);
-    computed += (computed << 15);
-    
-    ret.hash = computed;
-    return ret;
+	uint32_t computed, i;
+	for(computed = i = 0; i < len; i++)
+	{
+		computed += ptr[i];
+		computed += (computed << 10);
+		computed ^= (computed >> 6);
+	}
+	computed += (computed << 3);
+	computed ^= (computed >> 11);
+	computed += (computed << 15);
+
+	ret.hash = computed;
+	return ret;
 }
