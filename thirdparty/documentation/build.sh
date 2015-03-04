27b29d361f615a394a73bf971bf0dfea34cd8650
Kate Adams <self@kateadams.eu>
2015-03-04 20:09:14 +0000

    documentation: build: Added epub generator.

--- thirdparty/documentation/build.sh
+++ thirdparty/documentation/build.sh
@@ -47,4 +47,70 @@ make_pdf () {
 	cd -
 }
 
+make_epub () {
+	make_md
+	set -x # show the commands ran
+	cd ./build
+	ebook-convert ./cflare.md ./cflare.epub \
+		--title="CFlare Documentation" \
+		--authors="Kate Adams; Victor Meriqui" \
+		--cover images/cover.png --preserve-cover-aspect-ratio \
+		--chapter-mark=none \
+		--use-auto-toc \
+		--chapter="//*[((name()='h1' or name()='h2'or name()='h3')]" \
+		--page-breaks-before="//*[name()='h1']" \
+		--change-justification=justify \
+		--epub-inline-toc \
+		--level1-toc="//*[name()='h1']" \
+		--level2-toc="//*[name()='h2']" \
+		--level3-toc="//*[name()='h3']" \
+	#	--verbose
+	set +x
+	
+	# we need to fix up some stuff that ebook-convert does wrong/we don't like.
+	echo "unpacking epub..."
+	unzip ./cflare.epub -d ./cflare > /dev/null && rm ./cflare.epub
+		
+	# ids may not start with a hyphen, only alpha
+	# ebook-convert will only remove the first hyphen, so we need to fix it.
+	echo "fixing hyperlinks..."
+	# id="-abc" -> id="abc"
+	# then href="#-abc" -> href="#abc"
+	sed -i "s/\"-/\"/g" ./cflare/*.*html
+	sed -i "s/#-/#/g"   ./cflare/*.*html
+	
+	sed -i "s/\"-/\"/g" ./cflare/*.ncx
+	sed -i "s/#-/#/g"   ./cflare/*.ncx
+	
+	sed -i "s/\"-/\"/g" ./cflare/*.opf
+	sed -i "s/#-/#/g"   ./cflare/*.opf
+	
+	# modify the stylesheet
+	echo "updating stylesheet..."
+	echo "
+h1, h2, h3, h4, h5, h6 {
+	text-align: left;
+	text-indent: -1em;
+	padding-left: 1em;
+}
+code {
+	text-align: left;
+}
+p {
+	text-align: justify;
+}" >> ./cflare/stylesheet.css
+	
+	# now rebuild the epub
+	echo "repacking epub..."
+	cd ./cflare
+		zip -X   ../cflare.epub mimetype      > /dev/null
+		zip -grX ../cflare.epub META-INF/ *.* > /dev/null
+	cd -
+	
+	# now check that it's sane
+	epubcheck ./cflare.epub
+	
+	cd -
+}
+
 make_$1
