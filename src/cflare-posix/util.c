a6ed243301c6ff77e0ebbaa2ee8262f129f57b41
Kate Adams <self@kateadams.eu>
2015-03-31 20:03:08 +0100

    Posix's monotonic time (cflare_time()) starts from 0 upon application launch.

--- src/cflare-posix/util.c
+++ src/cflare-posix/util.c
@@ -3,6 +3,9 @@
 
 #include <time.h>
 
+bool first = true;
+float64_t start_time = -1;
+
 float64_t cflare_time()
 {
 #	ifdef TIME_UTC
@@ -12,8 +15,16 @@ float64_t cflare_time()
 #		else
 			timespec_get(&ts, TIME_UTC);
 #		endif
-		return (float64_t)ts.tv_sec + (float64_t)ts.tv_nsec / 1000.0 / 1000.0 / 1000.0;
+		
+		float64_t t = (float64_t)ts.tv_sec + (float64_t)ts.tv_nsec / 1000.0 / 1000.0 / 1000.0;
+		if(first)
+		{
+			first = false;
+			start_time = t;
+		}
+		return t - start_time;
 #	else
 		cflare_notimp();
+		return -1;
 #	endif
 }
